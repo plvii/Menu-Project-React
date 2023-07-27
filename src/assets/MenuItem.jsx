379d24142db2001f572621cbf8654a3e9636e2ce
plvii <1pallavithakur9@gmail.com>
2023-07-28 01:30:48 +0530

    menu project done

---
+++ src/assets/MenuItem.jsx
@@ -0,0 +1,9 @@
+// const MenuItem = ({ img, title, price, desc }) => {
+//   return (
+//     <article>
+//       <img src={img} alt={title} className="img" />
+//     </article>
+//   );
+// };
+
+// export default MenuItem;
